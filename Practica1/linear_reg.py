3e0d7d85a0d5a592b6b73bd0a292f564ae10b2b4
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2022-09-29 00:50:19 +0200

    clase

--- Practica1/linear_reg.py
+++ Practica1/linear_reg.py
@@ -16,9 +16,20 @@ def compute_cost(x, y, w, b):
         w, b (scalar): Parameters of the model
 
     Returns
-        total_cost (float): The cost of using w,b as the parameters for linear regression
+        total_cost (float): The cost of using w, b as the parameters for linear regression
                to fit the data points in x and y
     """
+
+    m = x.shape[0]
+
+    cost_sum = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        cost = (f_wb - y[i]) ** 2
+        cost_sum += cost
+
+    total_cost = (1 / (2 * m)) * cost_sum
 
     return total_cost
 
@@ -36,7 +47,23 @@ def compute_gradient(x, y, w, b):
     Returns
       dj_dw (scalar): The gradient of the cost w.r.t. the parameters w
       dj_db (scalar): The gradient of the cost w.r.t. the parameter b     
-     """
+    """
+
+    m = x.shape[0]
+
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w * x[i] + b
+        dj_dw_i = (f_wb - y[i]) * x[i]
+        dj_db_i = (f_wb - y[i])
+
+        dj_dw += dj_dw_i
+        dj_db += dj_db_i
+
+    dj_dw /= m
+    dj_db /= m
 
     return dj_dw, dj_db
 
@@ -57,6 +84,7 @@ def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, 
       gradient_function: function to compute the gradient
       alpha : (float) Learning rate
       num_iters : (int) number of iterations to run gradient descent
+
     Returns
       w : (ndarray): Shape (1,) Updated values of parameters of the model after
           running gradient descent
@@ -65,5 +93,25 @@ def gradient_descent(x, y, w_in, b_in, cost_function, gradient_function, alpha, 
       J_history : (ndarray): Shape (num_iters,) J at each iteration,
           primarily for graphing later
     """
+
+    m = len(x)
+
+    J_history = []
+    w = copy.deepcopy(w_in)
+    b = b_in
+
+    for i in range(m):
+        dj_dw, dj_db = gradient_function(x, y, w, b)
+
+        w -= alpha * dj_dw
+        b -= alpha * dj_db
+
+    for i in 100000:
+        cost = cost_function(x, y, w, b)
+        J_history.append(cost)
+
+    # e = 0
+    # for i in math.ceil(num_iters, e) :
+    #     print()   
 
     return w, b, J_history
