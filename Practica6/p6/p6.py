efe33a0ff374fa545f69cee5d6f3b44ff0a0b0a3
TomasLopezAnton <tomalope@ucm.es>
2022-11-17 17:56:00 +0100

    p6 mas o menos, mas menos que mas

--- Practica6/p6/p6.py
+++ Practica6/p6/p6.py
@@ -1,8 +1,9 @@
 import numpy as np
+import utils as ut
 import sklearn.model_selection as skm
 import sklearn.preprocessing as skp
 import sklearn.linear_model as skl
-
+import matplotlib.pyplot as plt
 
 def gen_data(m, seed=1, scale=0.7):
     # generate a data set based on a x^2 with added noise
@@ -15,30 +16,40 @@ def gen_data(m, seed=1, scale=0.7):
     return x_train, y_train, x_ideal, y_ideal
 
 def main():
-    X, y, x_ideal, y_ideal = gen_data(65)
+    X, y, x_ideal, y_ideal = gen_data(64)
+    m = X.shape[0]
+
+    plt.figure()
+    plt.scatter(X, y, c='blue', label='train')
+    plt.plot(x_ideal, y_ideal, c='red', label='y_ideal')
 
     X = X[:, None]
-    m = X.shape[0]
 
     X_train, X_test, y_train, y_test = skm.train_test_split(X, y, test_size=0.33, random_state=1)
 
     poly = skp.PolynomialFeatures(degree=15, include_bias=False)
-    poly.fit_transform(X_train)
-    poly.fit_transform(X_test)
+    X_train = poly.fit_transform(X_train)
+    X_test = poly.transform(X_test)
 
     std = skp.StandardScaler()
-    std.fit_transform(X_train)
-    std.fit_transform(X_test)
+    X_train = std.fit_transform(X_train)
+    X_test = std.transform(X_test)
 
     linear = skl.LinearRegression()
-    linear.fit(X_train)
+    linear.fit(X_train, y_train)
 
-    error_train = (linear.predict(X_train) - y)**2 / (2*m)
-    error_test = (linear.predict(X_test) - y)**2 / (2*m)
+    error_train = np.sum((linear.predict(X_train) - y_train)**2) / (2*m)
+    error_test = np.sum((linear.predict(X_test) - y_test)**2) / (2*m)
 
     print(error_train)
     print(error_test)
 
+    # X1 = np.sort(X)
+    # Y1 = np.sort(Y)
+
+    # plt.plot(X1, Y1, c='yellow', label='predict')
+    plt.legend()
+    plt.savefig('graph.pdf')
 
     print("\033[0m", end = '')
 
--- Practica6/p6/utils.py
+++ Practica6/p6/utils.py
@@ -1,136 +1,39 @@
-import matplotlib.pyplot as plt
 import numpy as np
+import matplotlib.pyplot as plt
 
+def gen_data(m, seed=1, scale=0.7):
+    """ generate a data set based on a x^2 with added noise """
+    c = 0
+    x_train = np.linspace(0, 49, m)
+    np.random.seed(seed)
+    y_ideal = x_train**2 + c
+    y_train = y_ideal + scale * y_ideal*(np.random.sample((m,)) - 0.5)
+    x_ideal  = x_train
+    return x_train, y_train, x_ideal, y_ideal
 
-###########################################################################
-# data display
-#
-def displayData(X):
-    num_plots = int(np.size(X, 0)**.5)
-    fig, ax = plt.subplots(num_plots, num_plots, sharex=True, sharey=True)
-    plt.subplots_adjust(left=0, wspace=0, hspace=0)
-    img_num = 0
-    for i in range(num_plots):
-        for j in range(num_plots):
-            # Convert column vector into 20x20 pixel matrix
-            # transpose
-            img = X[img_num, :].reshape(20, 20).T
-            ax[i][j].imshow(img, cmap='Greys')
-            ax[i][j].set_axis_off()
-            img_num += 1
+def plot_data(x_ideal, y_train, y_ideal):
+    """ plot the data, ideal red dotted line, train blue dots """
+    plt.figure()
+    plt.plot(x_ideal, y_ideal, 'r--', label='y_ideal')
+    plt.plot(x_ideal, y_train, 'bo', label='train')
+    plt.legend()
+    plt.show()
 
-    return (fig, ax)
+def plot_data_comparing(x_ideal, y_ideal, x_train, y_train, x_pred, y_pred):
+    """ plot the data, ideal red dotted line, train blue dots, pred blue line """
+    plt.figure()
+    plt.plot(x_train, y_train, 'bo', label='train', markersize=1)
+    plt.plot(x_pred, y_pred, 'b-', label='predicted')
+    plt.plot(x_ideal, y_ideal, 'r--', label='y_ideal')
+    plt.legend()
+    plt.show()
 
-
-def displayImage(im):
-    fig2, ax2 = plt.subplots()
-    image = im.reshape(20, 20).T
-    ax2.imshow(image, cmap='gray')
-    return (fig2, ax2)
-
-
-###########################################################################
-# gradient checking
-#
-def debugInitializeWeights(fan_in, fan_out):
-    """
-    Initializes the weights of a layer with fan_in incoming connections and
-    fan_out outgoing connections using a fixed set of values.
-    """
-
-    W = np.sin(np.arange(1, 1 + (1+fan_in)*fan_out))/10.0
-    W = W.reshape(fan_out, 1+fan_in, order='F')
-    return W
-
-
-def computeNumericalGradient(J, Theta1, Theta2):
-    """
-    Computes the gradient of J around theta using finite differences and
-    yields a numerical estimate of the gradient.
-    """
-
-    theta = np.append(Theta1, Theta2).reshape(-1)
-
-    numgrad = np.zeros_like(theta)
-    perturb = np.zeros_like(theta)
-    tol = 1e-4
-
-    for p in range(len(theta)):
-        # Set perturbation vector
-        perturb[p] = tol
-        loss1 = J(theta - perturb)
-        loss2 = J(theta + perturb)
-
-        # Compute numerical gradient
-        numgrad[p] = (loss2 - loss1) / (2 * tol)
-        perturb[p] = 0
-
-    return numgrad
-
-
-def checkNNGradients(costNN, reg_param=0):
-    """
-    Creates a small neural network to check the backpropagation gradients. It will output the
-    analytical gradients produced by your backprop code and the numerical gradients
-    (computed using computeNumericalGradient). These two gradient computations should result in
-    very similar values.
-    Parameters
-    ----------
-    nnCostFunction : func
-        A reference to the cost function implemented by the student.
-    reg_param : float (optional)
-        The regularization parameter value.
-    """
-
-    # Set up small NN
-    input_layer_size = 3
-    hidden_layer_size = 5
-    num_labels = 3
-    m = 5
-
-    # Generate some random test data
-    Theta1 = debugInitializeWeights(input_layer_size, hidden_layer_size)
-    Theta2 = debugInitializeWeights(hidden_layer_size, num_labels)
-
-    # Reusing debugInitializeWeights to get random X
-    X = debugInitializeWeights(input_layer_size - 1, m)
-
-    # Set each element of y to be in [0,num_labels]
-    y = [(i % num_labels) for i in range(m)]
-
-    ys = np.zeros((m, num_labels))
-    for i in range(m):
-        ys[i, y[i]] = 1
-
-    # Unroll parameters
-    # nn_params = np.append(Theta1, Theta2).reshape(-1)
-
-    # Compute Gradient
-    cost, grad1, grad2 = costNN(Theta1, Theta2,
-                                X, ys, reg_param)
-    grad = np.concatenate((np.ravel(grad1), np.ravel(grad2)))
-
-    def reduced_cost_func(p):
-        """ Cheaply decorated nnCostFunction """
-        Theta1 = np.reshape(
-            p[:hidden_layer_size * (input_layer_size + 1)],
-            (hidden_layer_size, (input_layer_size + 1)))
-        Theta2 = np.reshape(
-            p[hidden_layer_size * (input_layer_size + 1):],
-            (num_labels, (hidden_layer_size + 1)))
-        return costNN(Theta1, Theta2,
-                      X, ys, reg_param)[0]
-
-    numgrad = computeNumericalGradient(reduced_cost_func, Theta1, Theta2)
-
-    # Check two gradients
-    # np.testing.assert_almost_equal(grad, numgrad)
-
-    # Evaluate the norm of the difference between two the solutions. If you have a correct
-    # implementation, and assuming you used e = 0.0001 in computeNumericalGradient, then diff
-    # should be less than 1e-9.
-    diff = np.linalg.norm(numgrad - grad)/np.linalg.norm(numgrad + grad)
-
-    print('If your backpropagation implementation is correct, then \n'
-          'the relative difference will be small (less than 1e-9). \n'
-          'Relative Difference: %g' % diff)
+def plotCurvaAprendijaje(X, trainError, valError, linewidth=4):
+    """ Plot the learning curve """
+    plt.figure()
+    plt.plot(X, trainError, 'b-', label='train', linewidth=linewidth)
+    plt.plot(X, valError, '-', label='val', color='orange', linewidth=linewidth)
+    plt.ylabel('Error')
+    plt.xlabel('Number of Examples (m)')
+    plt.legend()
+    plt.show()
