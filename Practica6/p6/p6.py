d073dce2257ae0a819e6d7b1494ca8ca2f670864
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2023-01-03 19:40:31 +0100

    Refactorización p6

--- Practica6/p6/p6.py
+++ Practica6/p6/p6.py
@@ -1,20 +1,18 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import scipy.interpolate as scp
+import utils as ut
 
-import sklearn.model_selection as skm
+import numpy as np
+
+import sklearn.model_selection as skms
 import sklearn.preprocessing as skp
 import sklearn.linear_model as skl
 
-def gen_data(m, seed=1, scale=0.7):
-    # generate a data set based on a x^2 with added noise
-    c = 0
-    x_train = np.linspace(0, 49, m)
-    np.random.seed(seed)
-    y_ideal = x_train**2 + c
-    y_train = y_ideal + scale * y_ideal*(np.random.sample((m,))-0.5)
-    x_ideal = x_train
-    return x_train, y_train, x_ideal, y_ideal
+def calc_error(poly, scalar, model, x, y):
+    x_test = poly.transform(x)
+    x_test = scalar.transform(x_test)
+
+    y_predict = model.predict(x_test)
+
+    return np.sum((y_predict - y)**2) / (2 * y.shape[0]), x_test, y_predict[:, None]
 
 def train(x_train, y_train, degree=15, lambda_=0):
     poly = skp.PolynomialFeatures(degree=degree, include_bias=False)
@@ -28,57 +26,18 @@ def train(x_train, y_train, degree=15, lambda_=0):
 
     return poly, scalar, model
 
-def calc_error(poly, scalar, model, x, y):
-    x_test = poly.transform(x)
-    x_test = scalar.transform(x_test)
-
-    y_predict = model.predict(x_test)
-
-    return np.sum((y_predict - y)**2) / (2 * y.shape[0]), x_test, y_predict[:, None]
-
-def plot_train(X, y, x_ideal, y_ideal, X_predict, y_predict, fileName):
-    plt.figure()
-
-    plt.scatter(X, y, c='blue', label='train')
-    plt.plot(x_ideal, y_ideal, c='red', label='y_ideal', linestyle='dashed')
-
-    y_sorted = [y for _,y in sorted(zip(X_predict, y_predict))]
-    x_sorted = np.sort(X_predict, axis=None)
-
-    x_sorted_soften = np.linspace(x_sorted.min(), x_sorted.max(), 1000)
-    spline = scp.make_interp_spline(x_sorted, y_sorted)
-    y_sorted_soften = spline(x_sorted_soften)
-
-    plt.plot(x_sorted_soften, y_sorted_soften, c='black', label='predict', linestyle='dashed', linewidth=1)
-
-    plt.legend()
-    plt.savefig(fileName)
-    plt.close()
-
-def plot_error(num_data, error_data_train, error_data_cv, fileName, linewidth = 4):
-    plt.figure()
-
-    plt.xlabel("Num casos")
-    plt.ylabel("Error")
-
-    plt.plot(num_data, error_data_train, c='cyan', label='error_train', linewidth=3)
-    plt.plot(num_data, error_data_cv,c='orange', label='error_cv', linewidth=3)
-
-    plt.legend()
-    plt.savefig(fileName) # plt.show()
-    plt.close()
-
+# hiper parameters test
 def test_error():
     num_data = np.linspace(50, 1000, 20)
     error_data_train = np.zeros(len(num_data))
     error_data_cv = np.zeros(len(num_data))
 
     for value in range(len(num_data)):
-        X, y, x_ideal, y_ideal = gen_data(int(num_data[value]))
+        X, y, x_ideal, y_ideal = ut.gen_data(int(num_data[value]))
         X = X[:, None]
 
-        x_train, x_test, y_train, y_test = skm.train_test_split(X, y, test_size=0.4, random_state=1)
-        x_test, x_cv, y_test, y_cv = skm.train_test_split(x_test, y_test, test_size=0.5, random_state=1)
+        x_train, x_test, y_train, y_test = skms.train_test_split(X, y, test_size=0.4, random_state=1)
+        x_test, x_cv, y_test, y_cv = skms.train_test_split(x_test, y_test, test_size=0.5, random_state=1)
 
         degree = 16
         poly, scalar, model = train(x_train, y_train, degree)
@@ -86,11 +45,12 @@ def test_error():
         error_data_train[value], _, _ = calc_error(poly, scalar, model, x_train, y_train)
         error_data_cv[value], _, _ = calc_error(poly, scalar, model, x_cv, y_cv)
 
-    plot_error(num_data[1:], error_data_train[1:], error_data_cv[1:], './p6/docs/test_error.pdf')
+    ut.plot_error(num_data[1:], error_data_train[1:], error_data_cv[1:], './results/test_error.png')
 
+# hiper parameters test
 def test_hiper_parameters(X, y, x_ideal, y_ideal):
-    x_train, x_, y_train, y_ = skm.train_test_split(X, y, test_size=0.4, random_state=1)
-    x_test, x_cv, y_test, y_cv = skm.train_test_split(x_, y_, test_size=0.5, random_state=1)
+    x_train, x_, y_train, y_ = skms.train_test_split(X, y, test_size=0.4, random_state=1)
+    x_test, x_cv, y_test, y_cv = skms.train_test_split(x_, y_, test_size=0.5, random_state=1)
 
     degrees = 15
     lambdas = np.array([1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100, 300, 600, 900])
@@ -102,25 +62,29 @@ def test_hiper_parameters(X, y, x_ideal, y_ideal):
             error_data[temp_lambda, temp_degree], _, _ = calc_error(poly, scalar, model, x_cv, y_cv)
 
     min = np.argmin(error_data)
-    print("Min index:", min)
+    # print("Min index:", min)
 
     lambda_id = int(min/len(lambdas))
     proper_lambda = lambdas[lambda_id]
     proper_degree = min - (len(lambdas) * lambda_id) + 1 
+    
+    print("--HIPER PARAMETERS TEST--")
     print("Proper degree:", proper_degree)
     print("Proper lambda:", proper_lambda)
-    # print("Error degree&lambda:", error_data)
+    print("Error degree&lambda:", error_data)
 
     poly, scalar, model = train(x_train, y_train, proper_degree, proper_lambda)
 
     error, _, y_predict = calc_error(poly, scalar, model, x_test, y_test)
     print("Error test:", error)
+    print()
 
-    plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './p6/docs/test_hiper_parameters.pdf')
+    ut.plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './results/test_hiper_parameters.png')
 
+# lambda test
 def test_lambda(X, y, x_ideal, y_ideal):
-    x_train, x_, y_train, y_ = skm.train_test_split(X, y, test_size=0.4, random_state=1)
-    x_cv, x_test, y_cv, y_test = skm.train_test_split(x_, y_, test_size=0.5, random_state=1)
+    x_train, x_, y_train, y_ = skms.train_test_split(X, y, test_size=0.4, random_state=1)
+    x_cv, x_test, y_cv, y_test = skms.train_test_split(x_, y_, test_size=0.5, random_state=1)
 
     degree = 15
     lambdas = np.array([1e-6, 1e-5, 1e-4, 1e-3, 1e-2, 1e-1, 1, 10, 100, 300, 600, 900])
@@ -131,18 +95,22 @@ def test_lambda(X, y, x_ideal, y_ideal):
         error_data[temp_lambda], _, _ = calc_error(poly, scalar, model, x_cv, y_cv)
 
     proper_lambda = lambdas[np.argmin(error_data)] # MASSIVE PROBLEM, COLOSSAL EVEN
+    
+    print("--LAMBDA TEST--")
     print("Proper lambda:", proper_lambda)
-    # print("Error lambda:", error_data)
+    print("Error lambda:", error_data)
+    print()
 
     poly, scalar, model = train(x_train, y_train, degree, proper_lambda)
 
     _, _, y_predict = calc_error(poly, scalar, model, x_test, y_test)
 
-    plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './p6/docs/test_lambda.pdf')
+    ut.plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './results/test_lambda.png')
 
+# degree test
 def test_degree(X, y, x_ideal, y_ideal):
-    x_train, x_, y_train, y_ = skm.train_test_split(X, y, test_size=0.4, random_state=1)
-    x_cv, x_test, y_cv, y_test = skm.train_test_split(x_, y_, test_size=0.5, random_state=1)
+    x_train, x_, y_train, y_ = skms.train_test_split(X, y, test_size=0.4, random_state=1)
+    x_cv, x_test, y_cv, y_test = skms.train_test_split(x_, y_, test_size=0.5, random_state=1)
 
     error_data = np.zeros(10) # degree from 1 to 10 [(0,9) + 1]
     for temp_degree in range(len(error_data)):
@@ -151,45 +119,47 @@ def test_degree(X, y, x_ideal, y_ideal):
         error_data[temp_degree], _, _ = calc_error(poly, scalar, model, x_cv, y_cv)
 
     proper_degree = np.argmin(error_data) + 1
+
+    print("--DEGREE TEST--")
     print("Proper degree:", proper_degree)
-    # print("Error degree:", error_data)
+    print("Error degree:", error_data)
+    print()
 
     poly, scalar, model = train(x_train, y_train, proper_degree)
 
     _, _, y_predict = calc_error(poly, scalar, model, x_test, y_test)
 
-    plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './p6/docs/test_degree.pdf')
+    ut.plot_train(X, y, x_ideal, y_ideal, x_test, y_predict, './results/test_degree.png')
 
-
+# first test
 def test_start(X, y, x_ideal, y_ideal):
-    x_train, x_test, y_train, y_test = skm.train_test_split(X, y, test_size=0.33, random_state=1)
+    x_train, x_test, y_train, y_test = skms.train_test_split(X, y, test_size=0.33, random_state=1)
 
     poly, scalar, model = train(x_train, y_train)
 
     error_train, _, y_train_predict = calc_error(poly, scalar, model , x_train, y_train)
     error_test, _, _ = calc_error(poly, scalar, model, x_test, y_test)
 
+    print("--START TEST--")
     print("Error train:", error_train)
     print("Error test:", error_test)
+    print()
 
-    plot_train(X, y, x_ideal, y_ideal, x_train, y_train_predict, './p6/docs/test_start.pdf')
-
+    ut.plot_train(X, y, x_ideal, y_ideal, x_train, y_train_predict, './results/test_start.png')
 
 def main():
-    X, y, x_ideal, y_ideal = gen_data(64)
+    X, y, x_ideal, y_ideal = ut.gen_data(64)
     X = X[:, None]
 
     test_start(X, y, x_ideal, y_ideal)
     test_degree(X, y, x_ideal, y_ideal)
     test_lambda(X, y, x_ideal, y_ideal)
 
-    X, y, x_ideal, y_ideal = gen_data(750)
+    X, y, x_ideal, y_ideal = ut.gen_data(750)
     X = X[:, None]
     test_hiper_parameters(X, y, x_ideal, y_ideal)
 
     test_error()
-
-    print("\033[0m", end = '')
 
 if __name__ == '__main__':
     main()
---
+++ Practica6/p6/utils.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.interpolate as scp
+
+def gen_data(m, seed=1, scale=0.7):
+    # generate a data set based on a x^2 with added noise
+    c = 0
+    x_train = np.linspace(0, 49, m)
+    np.random.seed(seed)
+    y_ideal = x_train**2 + c
+    y_train = y_ideal + scale * y_ideal*(np.random.sample((m,))-0.5)
+    x_ideal = x_train
+    return x_train, y_train, x_ideal, y_ideal
+
+def plot_train(X, y, x_ideal, y_ideal, X_predict, y_predict, fileName):
+    plt.figure()
+
+    plt.scatter(X, y, c='blue', label='train')
+    plt.plot(x_ideal, y_ideal, c='red', label='y_ideal', linestyle='dashed')
+
+    y_sorted = [y for _,y in sorted(zip(X_predict, y_predict))]
+    x_sorted = np.sort(X_predict, axis=None)
+
+    x_sorted_soften = np.linspace(x_sorted.min(), x_sorted.max(), 1000)
+    spline = scp.make_interp_spline(x_sorted, y_sorted)
+    y_sorted_soften = spline(x_sorted_soften)
+
+    plt.plot(x_sorted_soften, y_sorted_soften, c='black', label='predict', linestyle='dashed', linewidth=1)
+
+    plt.legend()
+    plt.savefig(fileName)
+    plt.close()
+
+def plot_error(num_data, error_data_train, error_data_cv, fileName, linewidth = 4):
+    plt.figure()
+
+    plt.xlabel("Num casos")
+    plt.ylabel("Error")
+
+    plt.plot(num_data, error_data_train, c='cyan', label='error_train', linewidth=3)
+    plt.plot(num_data, error_data_cv,c='orange', label='error_cv', linewidth=3)
+
+    plt.legend()
+    plt.savefig(fileName) # plt.show()
+    plt.close()
