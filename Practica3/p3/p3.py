ce225c8f288cb42254472da4d6362e03e4e6ae5e
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2023-01-05 22:17:46 +0100

    Correccion p3

--- Practica3/p3/p3.py
+++ Practica3/p3/p3.py
@@ -11,10 +11,11 @@ def compute_logistic_reg_gradient_descent(X_data, y_data, lambda_=None):
     w_in, b_in = np.zeros(X_train.shape[1]), -8 if lambda_ is None else 1
 
     alpha = 0.001 if lambda_ is None else 0.01
+    compute_cost, compute_grad = (lgr.compute_cost, lgr.compute_gradient) if lambda_ is None else (lgr.compute_cost_reg, lgr.compute_gradient_reg)
     num_iters = 10000
 
     # we obtain w and b here
-    w, b, costs = lgr.gradient_descent(X_train, y_train, w_in, b_in, lgr.compute_cost, lgr.compute_gradient, alpha, num_iters)
+    w, b, costs = lgr.gradient_descent(X_train, y_train, w_in, b_in, compute_cost, compute_grad, alpha, num_iters, lambda_)
 
     return w, b, costs
 
@@ -35,7 +36,7 @@ def main():
     # parameters acquisition and visualization (regularized)
     X_data, y_data = ut.load_data('./data/ex2data2.txt')
     data = ut.map_data_features(X_data, y_data) # map features 
-    w, b, _ = compute_logistic_reg_gradient_descent(data[:,:-1], data[:,-1], 1)
+    w, b, _ = compute_logistic_reg_gradient_descent(data[:,:-1], data[:,-1], 0.1)
     ut.plot_logistic_reg("./results/logistic_reg_fit_regularized.png", data[:,:-1], data[:,-1], w, b)
 
 if __name__ == '__main__':
