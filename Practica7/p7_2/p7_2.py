813f58fbf243c9431ffd0857e185cb08f780b532
Psikotruko <62306290+SrVonPsikerfy@users.noreply.github.com>
2023-01-06 12:28:38 +0100

    Cambios p7.2

--- Practica7/p7_2/p7_2.py
+++ Practica7/p7_2/p7_2.py
@@ -13,15 +13,18 @@ def obtain_svm(x_train, y_train, x_val, y_val):
     
     # check for best parameters
     svm_ = None
+    c = sigma = 0
     best_acc = -1 
-    for c in values:
-        for sigma in values:
-            new_svm, _, accuracy = svm.svm(x_train, y_train, x_val, y_val, 'rbf', c, sigma)
+    for temp_c in values:
+        for temp_sigma in values:
+            new_svm, _, accuracy = svm.svm(x_train, y_train, x_val, y_val, 'rbf', temp_c, temp_sigma)
             
-            if (best_acc < accuracy):
+            if best_acc < accuracy:
                 best_acc = accuracy
-                svm_ = new_svm
-                
+                svm_, c, sigma = new_svm, temp_c, temp_sigma
+    
+    print("SVM best parameters: C --> " + str(c) + "; Sigma --> " + str(sigma))
+
     return svm_
 
 def obtain_reg_log(x_train, y_train, x_val, y_val):
@@ -40,14 +43,16 @@ def obtain_reg_log(x_train, y_train, x_val, y_val):
         predict = lgr.predict(x_val, new_w, new_b)
         accuracy = np.sum(y_val == predict) / y_val.shape[0]
 
-        if(best_acc < accuracy):
+        if best_acc < accuracy:
             best_acc = accuracy
             lambda_ = temp_lambda
+
+    print("Logistic regression best parameters: Lambda --> " + str(lambda_))
 
     # redo best regression with more iterations
     num_iters = 10000
     w, b, _ = lgr.gradient_descent(x_train, y_train, w_in, b_in, lgr.compute_cost_reg, lgr.compute_gradient_reg, alpha, num_iters, lambda_)
-
+    
     return w, b
 
 def obtain_neural_network(x_train, y_train, x_val, y_val):
@@ -56,7 +61,7 @@ def obtain_neural_network(x_train, y_train, x_val, y_val):
     values = [1e-5, 1e-4, 0.001, 0.01, 0.1, 1, 10, 50]
 
     output_labels = 2               # spam or not spam
-    input_labels = len(x_train[0])  # list words email
+    input_labels = len(x_train[0])  # nº words email
     hidden_labels = 25              # because I can
 
     theta1, theta2 = nn.random_thetas(input_labels, hidden_labels, output_labels)
@@ -72,11 +77,13 @@ def obtain_neural_network(x_train, y_train, x_val, y_val):
         predict = ut.predict(theta_1, theta_2, x_val)
         accuracy = np.sum(y_val == predict) / y_val.shape[0]
 
-        print('Lambda: ', temp_lambda)
+        print('Lambda:', temp_lambda)
 
-        if(best_acc < accuracy):
+        if best_acc < accuracy:
             best_acc = accuracy
             lambda_ = temp_lambda
+
+    print("Neural network best parameters: Lambda --> " + str(lambda_))
 
     # redo best neural network with more iterations
     num_iters = 1000
@@ -87,30 +94,34 @@ def obtain_neural_network(x_train, y_train, x_val, y_val):
 def check_spam_svm(x_train, y_train, x_val, y_val, x_test, y_test):
     svm = obtain_svm(x_train, y_train, x_val, y_val)
     accuracy = skme.accuracy_score(y_test, svm.predict(x_test))
-    print("SVM best accuracy: ", accuracy)
+    print("SVM accuracy:", round(accuracy * 100, 2))
 
 def check_spam_reg_log(x_train, y_train, x_val, y_val, x_test, y_test):
     w, b = obtain_reg_log(x_train, y_train, x_val, y_val)
     predict = lgr.predict(x_test, w, b)
     accuracy = np.sum(y_test == predict) / y_test.shape[0]
-    print("Logistic regression best accuracy: ", accuracy)
+    print("Logistic regression accuracy:", round(accuracy * 100, 2))
     
 def check_spam_neural_network(x_train, y_train, x_val, y_val, x_test, y_test):
     theta1, theta2 = obtain_neural_network(x_train, y_train, x_val, y_val)
     p = ut.predict(theta1, theta2, x_test)
     accuracy = np.sum(y_test == p) / y_test.shape[0]
-    print("Neural network best accuracy: ", accuracy)
+    print("Neural network accuracy:", round(accuracy * 100, 2))
 
 def main(training_sys):
     x_train, y_train, x_val, y_val, x_test, y_test = ut.preprocess_data()
     print("Preload finished.")
 
-    if training_sys == 'svm':       # SVM accuracy:  0.9803030303030303
+    if training_sys == 'svm':       # SVM accuracy: 98.03
         check_spam_svm(x_train, y_train, x_val, y_val, x_test, y_test)
-    elif training_sys == 'nn':      # Neural network accuracy:  0.9712121212121212
+    elif training_sys == 'nn':      # Neural network accuracy: 96.97
         check_spam_neural_network(x_train, y_train, x_val, y_val, x_test, y_test)
-    elif training_sys == 'log':     # Logistic regression accuracy:  0.9742424242424242
+    elif training_sys == 'log':     # Logistic regression accuracy: 97.42
         check_spam_reg_log(x_train, y_train, x_val, y_val, x_test, y_test)
+    elif training_sys == 'all':
+        check_spam_reg_log(x_train, y_train, x_val, y_val, x_test, y_test)
+        check_spam_neural_network(x_train, y_train, x_val, y_val, x_test, y_test)
+        check_spam_svm(x_train, y_train, x_val, y_val, x_test, y_test)
 
 if __name__ == '__main__':
-    main('nn')
+    main('log')
